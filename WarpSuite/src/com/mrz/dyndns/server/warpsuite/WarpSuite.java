3921222c98ea50e06d8caed1261d5987e110e5d5
MrZoraman <MrZoraman@Gmail.com>
2013-09-04 19:17:34 -0700

    Added commandSystem

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/WarpSuite.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/WarpSuite.java
@@ -2,17 +2,21 @@ package com.mrz.dyndns.server.warpsuite;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mrz.dyndns.server.CommandSystem.CommandSystem;
+
 public class WarpSuite extends JavaPlugin
 {
+	private CommandSystem cs;
+	
 	@Override
 	public void onEnable()
 	{
-		
+		cs = new CommandSystem(this);
 	}
 	
 	@Override
 	public void onDisable()
 	{
-		
+		cs.close();
 	}
 }
