b5d009d4e9b12fb7213f383bb79aa3bc97123f48
MrZoraman <MrZoraman@Gmail.com>
2013-09-04 19:44:49 -0700

    added MyConfig

---
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/util/MyConfig.java
@@ -0,0 +1,58 @@
+package com.mrz.dyndns.server.warpsuite.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyConfig 
+{
+	private FileConfiguration customConfig = null;
+	private File customConfigFile = null;
+	
+	private final String location;
+	private final JavaPlugin plugin;
+	
+	public MyConfig(String location, JavaPlugin plugin)
+	{
+		this.location = location;
+		this.plugin = plugin;
+	}
+	
+	public void reloadCustomConfig() {
+	    if (customConfigFile == null) {
+	    	customConfigFile = new File(plugin.getDataFolder(), location + ".yml");
+	    }
+	    customConfig = YamlConfiguration.loadConfiguration(customConfigFile);
+	 
+	    // Look for defaults in the jar
+	    InputStream defConfigStream = plugin.getResource(location + ".yml");
+	    if (defConfigStream != null) {
+	        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+	        customConfig.setDefaults(defConfig);
+	    }
+	}
+	
+	public FileConfiguration getCustomConfig() {
+	    if (customConfig == null) {
+	        this.reloadCustomConfig();
+	    }
+	    return customConfig;
+	}
+	
+	public void saveCustomConfig() {
+	    //Debug.print("Saving config " + location,"MyConfig","SaveCustomConfig");
+	    if (customConfig == null || customConfigFile == null) {
+	    return;
+	    }
+	    try {
+	        getCustomConfig().save(customConfigFile);
+	    } catch (IOException ex) {
+	        plugin.getLogger().log(Level.SEVERE, "Could not save config to " + customConfigFile, ex);
+	    }
+	}
+}
