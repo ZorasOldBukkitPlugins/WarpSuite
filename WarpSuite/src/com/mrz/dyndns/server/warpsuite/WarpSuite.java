c5be9e0b1defc0ee27fd444012551154fda50a46
MrZoraman <MrZoraman@Gmail.com>
2013-09-04 19:15:09 -0700

    Initial commit

---
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/WarpSuite.java
@@ -0,0 +1,18 @@
+package com.mrz.dyndns.server.warpsuite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WarpSuite extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
