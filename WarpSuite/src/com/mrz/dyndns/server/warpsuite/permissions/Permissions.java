4aba3ad46ea200dc38d73fb1880d998a920f998f
MrZoraman <MrZoraman@Gmail.com>
2013-09-05 19:19:52 -0700

    Added Permissions

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/permissions/Permissions.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/permissions/Permissions.java
@@ -4,8 +4,32 @@ import org.bukkit.entity.Player;
 
 public enum Permissions
 {
-	HELP("warpsuite.help"),
-	WARP("warpsuite.warp");
+	//user
+	HELP			("warpsuite.help"),
+	WARP			("warpsuite.warp"),
+	WARP_SET		("warpsuite.warp.set"),
+	WARP_REMOVE		("warpsuite.warp.remove"),
+	WARP_LIST		("warpsuite.warp.list"),
+	
+	//admin
+	ADMIN_WARP		("warpsuite.admin.warp"),
+	ADMIN_SENDTO	("warpsuite.admin.sendto"),
+	ADMIN_TOMY		("warpsuite.admin.tomy"),
+	ADMIN_SET		("warpsuite.admin.set"),
+	ADMIN_DELETE	("warpsuite.admin.delete"),
+	ADMIN_LIST		("warpsuite.admin.list"),
+	
+	//public
+	PUBLIC_WARP		("warpsuite.public.warp"),
+	PUBLIC_SET		("warpsuite.public.set"),
+	PUBLIC_DELETE	("warpsuite.public.delete"),
+	PUBLIC_LIST		("warpsuite.public.list"),
+	
+	//misc
+	WARP_INVITE		("warpsuite.warp.invite"),
+	COUNT_INFINITE	("warpsuite.count.infinite"),
+	DELAY_BYPASS	("warpsuite.delay.bypass");
+	
 	
 	private Permissions(String node)
 	{
