3e188327186f16945c755e434d7e20e2dd3b8c52
MrZoraman <MrZoraman@Gmail.com>
2013-09-06 16:50:57 -0700

    max warp checking

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/SetPlayersOwnWarp.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/SetPlayersOwnWarp.java
@@ -2,8 +2,11 @@ package com.mrz.dyndns.server.warpsuite.commands;
 
 import java.util.List;
 
+import org.bukkit.ChatColor;
+
 import com.mrz.dyndns.server.warpsuite.WarpSuite;
 import com.mrz.dyndns.server.warpsuite.WarpSuitePlayer;
+import com.mrz.dyndns.server.warpsuite.permissions.NumeralPermissions;
 
 public class SetPlayersOwnWarp extends WarpSuiteCommand
 {
@@ -16,6 +19,16 @@ public class SetPlayersOwnWarp extends WarpSuiteCommand
 	@Override
 	public boolean warpPlayerExecute(WarpSuitePlayer player, List<String> args, List<String> variables)
 	{
+		int allowedWarps = NumeralPermissions.COUNT.getAmount(player.getPlayer());
+		int warpsOwned = player.getWarpManager().getAmountOfSetWarps();
+		
+		if(allowedWarps != -1 && warpsOwned >= allowedWarps)
+		{
+			player.sendMessage(ChatColor.RED + "You already have the maximum warps you're allowed to set! (" + ChatColor.DARK_GRAY + allowedWarps + ChatColor.RED + ")");
+			return true;
+		}
+		
+		
 		
 		return false;
 	}
