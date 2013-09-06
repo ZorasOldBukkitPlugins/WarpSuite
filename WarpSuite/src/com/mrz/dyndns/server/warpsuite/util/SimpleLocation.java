6cc8059eadb238866b14cc12abba5a798074d3e9
MrZoraman <MrZoraman@Gmail.com>
2013-09-06 16:50:47 -0700

    constructor can take location object

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/util/SimpleLocation.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/util/SimpleLocation.java
@@ -1,5 +1,7 @@
 package com.mrz.dyndns.server.warpsuite.util;
 
+import org.bukkit.Location;
+
 public class SimpleLocation
 {
 	public SimpleLocation(double x, double y, double z, double yaw, double pitch)
@@ -9,6 +11,15 @@ public class SimpleLocation
 		this.Z = z;
 		this.Yaw = yaw;
 		this.Pitch = pitch;
+	}
+	
+	public SimpleLocation(Location loc)
+	{
+		this.X = loc.getX();
+		this.Y = loc.getY();
+		this.Z = loc.getZ();
+		this.Yaw = loc.getYaw();
+		this.Pitch = loc.getPitch();
 	}
 	
 	public final double X;
