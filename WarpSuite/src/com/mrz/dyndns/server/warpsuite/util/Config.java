ffe2b9ecaf5e30ab9a12ca84a20803f7121422d4
MrZoraman <MrZoraman@Gmail.com>
2013-09-06 19:44:10 -0700

    updated config

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/util/Config.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/util/Config.java
@@ -12,22 +12,24 @@ public final class Config
 	{
 		timer = config.getInt("timer", 10);
 		radius = config.getInt("radius", 10);
-		cancelOnDamage = config.getBoolean("cancelOnDamage", true);
+		cancelOnMobDamage = config.getBoolean("cancelOn.mobDamage", true);
+		cancelOnMove = config.getBoolean("cancelOn.move", true);
+		cancelOnPvp = config.getBoolean("cancelOn.playerPVP", true);
 		timerAndRadius = config.getBoolean("timerAndRadius", true);
 		globalWarpsCountTowardsTotal = config.getBoolean("globalWarpsCountTowardsTotal", false);
 		defaultMaxWarps = config.getInt("defaultMaxWarps", 50);
 		warpInviteTimeout = config.getInt("warpInviteTimeout", 120);
 		useMultiverse = config.getBoolean("useMultiverse", true);
-		cancelOnMove = config.getBoolean("cancelOnMove", true);
 	}
 	
 	public static int timer;
 	public static int radius;
-	public static boolean cancelOnDamage;
+	public static boolean cancelOnMobDamage;
 	public static boolean timerAndRadius;
 	public static boolean globalWarpsCountTowardsTotal;
 	public static int defaultMaxWarps;
 	public static int warpInviteTimeout;
 	public static boolean useMultiverse;
 	public static boolean cancelOnMove;
+	public static boolean cancelOnPvp;
 }
