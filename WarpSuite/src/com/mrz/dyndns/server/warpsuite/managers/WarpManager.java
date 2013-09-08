11bac2ae04ee0ff0f9bd351e8c09aa8dc872ac83
MrZoraman <MrZoraman@Gmail.com>
2013-09-08 15:14:00 -0700

    you don't need to sort that

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/managers/WarpManager.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/managers/WarpManager.java
@@ -1,7 +1,6 @@
 package com.mrz.dyndns.server.warpsuite.managers;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -115,7 +114,6 @@ public class WarpManager
 		{
 			warpList.add(config.getCustomConfig().getString("Warps." + warpName + ".ListingName"));
 		}
-		Collections.sort(warpList);
 		return warpList;
 	}
 	
