f50797b8831a8a13aaae2c031ad6cd37262e4576
MrZoraman <MrZoraman@Gmail.com>
2013-09-07 22:24:07 -0700

    check if there is such warp or not

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/admin/DeleteOtherPlayersWarp.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/admin/DeleteOtherPlayersWarp.java
@@ -9,6 +9,7 @@ import org.bukkit.command.ConsoleCommandSender;
 
 import com.mrz.dyndns.server.warpsuite.WarpSuite;
 import com.mrz.dyndns.server.warpsuite.commands.WarpSuiteCommand;
+import com.mrz.dyndns.server.warpsuite.managers.WarpManager;
 import com.mrz.dyndns.server.warpsuite.permissions.Permissions;
 import com.mrz.dyndns.server.warpsuite.players.OfflineWarpSuitePlayer;
 import com.mrz.dyndns.server.warpsuite.players.WarpSuitePlayer;
@@ -50,12 +51,18 @@ public class DeleteOtherPlayersWarp extends WarpSuiteCommand
 		OfflineWarpSuitePlayer target = new OfflineWarpSuitePlayer(targetName, plugin);
 		if(target.hasPlayedBefore() == false)
 		{
-			sender.sendMessage(NEGATIVE_PRIMARY + "Player \'" + NEGATIVE_SECONDARY + target + NEGATIVE_PRIMARY + "\' has not played on this server before!");
+			sender.sendMessage(NEGATIVE_PRIMARY + "Player \'" + NEGATIVE_SECONDARY + targetName + NEGATIVE_PRIMARY + "\' has not played on this server before!");
 			return true;
 		}
 		
-		String warpName = args.get(0);		
-		target.getWarpManager().removeWarp(warpName);
+		String warpName = args.get(0);
+		WarpManager manager = target.getWarpManager();
+		if(manager.warpIsSet(warpName) == false)
+		{
+			sender.sendMessage(NEGATIVE_PRIMARY + "Player \'" + NEGATIVE_SECONDARY + targetName 
+					+ NEGATIVE_PRIMARY + "\' does not have a warp called \'" + NEGATIVE_SECONDARY + warpName + NEGATIVE_PRIMARY + "\' set!");
+		}
+		manager.removeWarp(warpName);
 		sender.sendMessage(POSITIVE_PRIMARY + "Warp \'" + POSITIVE_SECONDARY + warpName + POSITIVE_PRIMARY + "\' has been removed from player \'" + POSITIVE_SECONDARY + targetName + POSITIVE_PRIMARY + "\'");
 		return true;
 	}
