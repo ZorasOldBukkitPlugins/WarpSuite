ee45450b26bcbe7c418f93df2dd4e5acb656da90
MrZoraman <MrZoraman@Gmail.com>
2013-09-07 22:05:19 -0700

    better messaging, verifies if warp is valid or not

--- WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/admin/SetOtherPlayersWarp.java
+++ WarpSuite/src/com/mrz/dyndns/server/warpsuite/commands/admin/SetOtherPlayersWarp.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.mrz.dyndns.server.warpsuite.WarpSuite;
 import com.mrz.dyndns.server.warpsuite.commands.WarpSuiteCommand;
+import com.mrz.dyndns.server.warpsuite.managers.WarpManager;
 import com.mrz.dyndns.server.warpsuite.permissions.Permissions;
 import com.mrz.dyndns.server.warpsuite.players.OfflineWarpSuitePlayer;
 import com.mrz.dyndns.server.warpsuite.players.WarpSuitePlayer;
@@ -37,16 +38,25 @@ public class SetOtherPlayersWarp extends WarpSuiteCommand
 		OfflineWarpSuitePlayer target = new OfflineWarpSuitePlayer(targetName, plugin);
 		String warpName = args.get(0);
 		
+		if(Util.isValidWarpName(warpName) == false)
+		{
+			player.sendMessage(NEGATIVE_PRIMARY + "\'" + NEGATIVE_SECONDARY + warpName + NEGATIVE_PRIMARY + "\' is an invalid warp name!");
+			return true;
+		}
+		
 		if(target.hasPlayedBefore() == false)
 		{
 			player.sendMessage(WARNING_PRIMARY + "Note: player \'" + WARNING_SECONDARY + targetName 
 					+ WARNING_PRIMARY + "\' has not played on this server before, so a new warp file has been made for him");
 		}
 		
+		WarpManager manager = target.getOrMakeWarpManager();
+		
 		SimpleLocation sLoc = new SimpleLocation(player.getPlayer().getLocation());
-		target.getOrMakeWarpManager().setWarp(warpName, sLoc);
+		boolean overWritten = manager.warpIsSet(warpName);
+		manager.setWarp(warpName, sLoc);
 		player.sendMessage(POSITIVE_PRIMARY + "Warp \'" + POSITIVE_SECONDARY + warpName + POSITIVE_PRIMARY 
-				+ "\' has been set for player \'" + POSITIVE_SECONDARY + targetName + POSITIVE_PRIMARY + "\'");
+				+ "\' has been " + (overWritten ? "overwritten" : "set") + " for player \'" + POSITIVE_SECONDARY + targetName + POSITIVE_PRIMARY + "\'");
 		
 		return true;
 	}
